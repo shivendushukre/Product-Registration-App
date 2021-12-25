ede3f431bc60f190be385c04b735997c674d56e1
shivendushukre <shivendu2711@gmail.com>
2021-12-25 21:03:27 +0530

    second commit

--- routers/db_router.py
+++ routers/db_router.py
@@ -1,5 +1,5 @@
 class AuthRouter:
-    route_app_labels = {'auth', 'contenttypes', 'sessions', 'admin'}
+    route_app_labels = {'auth', 'contenttypes', 'sessions', 'admin', 'users'}
 
     def db_for_read(self, model, **hints):
         if model._meta.app_label in self.route_app_labels:
@@ -16,7 +16,7 @@ class AuthRouter:
             obj1._meta.app_label in self.route_app_labels or
             obj2._meta.app_label in self.route_app_labels
         ):
-           return True
+            return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import PostUser
 
-admin.site.register(PostUser)
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+admin.site.register(PostUser, PostAdmin)
